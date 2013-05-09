f96a140a612d5278d74993151d00aca7e6e0daed
Jake Ginnivan <jake.ginnivan@readify.net>
2013-05-09 10:08:28 +0800

    Adding another fix for trx files which corrects start date being after endDate

--- TfsCreateBuild/TrxIdCorrector.cs
+++ TfsCreateBuild/TrxIdCorrector.cs
@@ -10,19 +10,21 @@ namespace TfsCreateBuild
 {
     class TrxIdCorrector
     {
+        const string Ns = "http://microsoft.com/schemas/VisualStudio/TeamTest/2010";
+
         public static void FixTestIdsInTrx(string testResults)
         {
-            const string ns = "http://microsoft.com/schemas/VisualStudio/TeamTest/2010";
             var trx = File.ReadAllText(testResults);
 
             var replaceList = new Dictionary<Guid, Guid>();
             var storageReplace = new Dictionary<string, string>();
 
             var doc = XDocument.Load(testResults);
-            var unitTests = doc.Descendants(XName.Get("TestRun", ns)).Descendants(XName.Get("TestDefinitions", ns)).Single().Descendants(XName.Get("UnitTest", ns));
+            FixEndDateBeforeStartDate(doc);
+            var unitTests = doc.Descendants(XName.Get("TestRun", Ns)).Descendants(XName.Get("TestDefinitions", Ns)).Single().Descendants(XName.Get("UnitTest", Ns));
             foreach (var unitTest in unitTests)
             {
-                var testMethod = unitTest.Descendants(XName.Get("TestMethod", ns)).Single();
+                var testMethod = unitTest.Descendants(XName.Get("TestMethod", Ns)).Single();
                 var storage = unitTest.Attribute("storage").Value;
                 if (!storageReplace.ContainsKey(storage))
                     storageReplace.Add(storage, Path.GetFileName(storage));
@@ -38,6 +40,23 @@ namespace TfsCreateBuild
             trx = storageReplace.Aggregate(trx, (current, replacement) => current.Replace(replacement.Key.ToString(), replacement.Value.ToString()));
             
             File.WriteAllText(testResults, trx);
+        }
+
+        private static void FixEndDateBeforeStartDate(XDocument doc)
+        {
+            var unitTests = doc.Descendants(XName.Get("TestRun", Ns)).Descendants(XName.Get("Results", Ns)).Single().Descendants(XName.Get("UnitTestResult", Ns));
+
+            foreach (var unitTest in unitTests)
+            {
+                var endTimeValue = unitTest.Attribute("endTime").Value;
+                var startTimeValue = unitTest.Attribute("startTime").Value;
+                var startTime = DateTime.Parse(startTimeValue);
+                var endTime = DateTime.Parse(endTimeValue);
+                if (startTime > endTime)
+                {
+                    unitTest.SetAttributeValue("startTime", endTime.Subtract(TimeSpan.Parse(unitTest.Attribute("duration").Value)));
+                }
+            }
         }
 
         static Guid CalcProperGuid(string testName)
